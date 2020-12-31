cd3b8b58f2757c8f22ccd0456bed37bb184af40f
hengchu <hengchu.zhang@gmail.com>
2020-12-31 15:52:37 -0500

    backup

--- src/Data/DPHS/Typecheck/Fuzzi.hs
+++ src/Data/DPHS/Typecheck/Fuzzi.hs
@@ -12,29 +12,47 @@ import Data.Comp.Multi.HFunctor
 import Data.Comp.Multi.Term hiding (Cxt)
 import Optics
 
+import Data.DPHS.Syntax
+import Data.DPHS.SrcLoc
+import Data.DPHS.Fuzzi
 import Data.DPHS.Name
 
 -- |Annotation for whether a program fragment is deterministic or probabilistic.
 data ProbAnn = D | P deriving (Show, Eq, Ord)
 
 -- |Annotation for whether a program fragment co-terminates or terminates.
-data TermAnn = C | T deriving (Show, Eq, Ord)
+data TermAnn = T | C deriving (Show, Eq, Ord)
+
+instance Monoid ProbAnn where
+  mempty = D
+
+instance Semigroup ProbAnn where
+  D <> D = D
+  _ <> _ = P
+
+instance Monoid TermAnn where
+  mempty = T
+
+instance Semigroup TermAnn where
+  T <> T = T
+  _ <> _ = C
 
 data Sensitivity =
-  Constant Double
-  | Sensitive Double
+  Const Double
+  | Sens Double
   deriving (Show, Eq, Ord)
 
-type TyContext = M.Map Name Sensitivity
+type Cxt = M.Map Name Sensitivity
 
 data TypeInfo =
   ExprInfo {
     tiSensitivity :: Sensitivity,
-    tiProbAnnn :: ProbAnn,
-    tiTermAnn :: TermAnn
+    tiProbAnn :: ProbAnn,
+    tiTermAnn :: TermAnn,
+    tiEpsilon :: Double
   }
   | CmdInfo {
-    tiPostContext :: TyContext,
+    tiPostContext :: Cxt,
     tiProbAnn :: ProbAnn,
     tiTermAnn :: TermAnn,
     tiEpsilon :: Double,
@@ -44,22 +62,87 @@ data TypeInfo =
 makeFieldLabelsWith abbreviatedFieldLabels ''TypeInfo
 makePrisms ''TypeInfo
 
-data MacroTypeInfo m =
+--- |An intermediate type information value, that may not be grounded.
+data InternalTypeInfo m =
   Atomic {
     -- |The type info of a grounded term.
-    mtiTypeInfo :: TypeInfo
+    itiTypeInfo :: TypeInfo
   }
   | Macro {
     -- |Delayed typechecker for a macro that is not yet instantiated.
-    mtiFunctionInfo :: MacroTypeInfo m -> MacroTypeInfo m
+    itiFunctionInfo :: TypeInfo -> m (InternalTypeInfo m)
   }
 
-makeFieldLabelsWith abbreviatedFieldLabels ''MacroTypeInfo
-makePrisms ''MacroTypeInfo
+makeFieldLabelsWith abbreviatedFieldLabels ''InternalTypeInfo
+makePrisms ''InternalTypeInfo
 
-type TypeChecker m = TyContext -> MacroTypeInfo m
+-- |A default expression type information that can be modified by lenses.
+exprInfo :: InternalTypeInfo m
+exprInfo = Atomic (ExprInfo (Sens 0) D T 0)
+
+atomicCmdInfo :: Cxt -> ProbAnn -> TermAnn -> Double -> Double -> InternalTypeInfo m
+atomicCmdInfo c p t e d = Atomic (CmdInfo c p t e d)
+
+-- |Cast an internal type info into an expression type info.
+asExprInfo :: InternalTypeInfo m -> Maybe (Sensitivity, ProbAnn, TermAnn, Double)
+asExprInfo info =
+  (,,,) <$> preview (#typeInfo % #sensitivity) info
+        <*> preview (#typeInfo % #probAnn) info
+        <*> preview (#typeInfo % #termAnn) info
+        <*> preview (#typeInfo % #epsilon) info
+
+-- |Cast an internal type info into a command type info
+asCmdInfo :: InternalTypeInfo m -> Maybe (Cxt, ProbAnn, TermAnn, Double, Double)
+asCmdInfo info =
+  (,,,,) <$> preview (#typeInfo % #postContext) info
+         <*> preview (#typeInfo % #probAnn) info
+         <*> preview (#typeInfo % #termAnn) info
+         <*> preview (#typeInfo % #epsilon) info
+         <*> preview (#typeInfo % #delta) info
+
+type TypeChecker m = Cxt -> m (InternalTypeInfo m)
 
 class TyAlg (h :: (* -> *) -> * -> *) where
   tyAlg :: MonadThrow m => Alg h (K (TypeChecker m))
+
+data TypeError =
+  ExpectingAnExpression
+  | BranchConditionProb
+  | BranchConditionSensitive
+  deriving (Show, Eq, Ord)
+
+data PositionAndTypeError = PTE {
+  _ptePosition :: Pos,
+  _pteTypeError :: TypeError
+  } deriving (Show, Eq)
+
+instance Exception PositionAndTypeError
+
+throwTE :: MonadThrow m => Pos -> TypeError -> m a
+throwTE p e = throwM (PTE p e)
+
+tyAlgEff :: MonadThrow m => Alg (EffF :&: Pos) (K (TypeChecker m))
+tyAlgEff (Assign (V x) rhs :&: position) = K $ \cxt -> do
+  tyRhs <- unK rhs cxt
+  case asExprInfo tyRhs of
+    Nothing -> throwTE position ExpectingAnExpression
+    Just (s, p, t, e) ->
+      let cxt' = M.insert x s cxt in
+      return (atomicCmdInfo cxt' p t e 0)
+tyAlgEff (Branch cond c1 c2 :&: position) = K $ \cxt -> do
+  tyCond <- unK cond cxt
+  case asExprInfo tyCond of
+    Just (Sens 0, D, t, 0) -> do
+      tyC1 <- unK c1 cxt
+      tyC2 <- unK c2 cxt
+      case (asCmdInfo tyC1, asCmdInfo tyC2) of
+        (Just _, Just _) ->
+          return undefined
+        _ -> undefined
+    Nothing -> throwTE position ExpectingAnExpression
+    Just (Sens _, _, _, _) ->
+      throwTE position BranchConditionSensitive
+    Just (_, P, _, _) ->
+      throwTE position BranchConditionProb
 
 liftSum ''TyAlg
