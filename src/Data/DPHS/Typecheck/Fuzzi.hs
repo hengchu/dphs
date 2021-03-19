5ffe896c1ac789a50b524757c22e1a37d970e5db
hengchu <hengchu.zhang@gmail.com>
2021-03-18 21:39:18 -0400

    backup

--- src/Data/DPHS/Typecheck/Fuzzi.hs
+++ src/Data/DPHS/Typecheck/Fuzzi.hs
@@ -66,9 +66,20 @@ instance Semigroup Sensitivity where
 instance Monoid Sensitivity where
   mempty = Sens 0
 
-type Cxt = M.Map Name Sensitivity
+type Cxt m = M.Map Name (InternalTypeInfo m)
 
-data TypeInfo =
+--- |An intermediate type information value, that may not be grounded.
+data InternalTypeInfo m =
+  Atomic {
+    -- |The type info of a grounded term.
+    itiTypeInfo :: TypeInfo m
+  }
+  | Macro {
+    -- |Delayed typechecker for a macro that is not yet instantiated.
+    itiFunctionInfo :: InternalTypeInfo m -> m (InternalTypeInfo m)
+  }
+
+data TypeInfo m =
   ExprInfo {
     tiSensitivity :: Sensitivity,
     tiProbAnn :: ProbAnn,
@@ -76,26 +87,15 @@ data TypeInfo =
     tiEpsilon :: Double
   }
   | CmdInfo {
-    tiPostContext :: Cxt,
+    tiPostContext :: Cxt m,
     tiProbAnn :: ProbAnn,
     tiTermAnn :: TermAnn,
     tiEpsilon :: Double,
     tiDelta   :: Double
-  } deriving (Show, Eq, Ord)
+  } 
 
 makeFieldLabelsWith abbreviatedFieldLabels ''TypeInfo
 makePrisms ''TypeInfo
-
---- |An intermediate type information value, that may not be grounded.
-data InternalTypeInfo m =
-  Atomic {
-    -- |The type info of a grounded term.
-    itiTypeInfo :: TypeInfo
-  }
-  | Macro {
-    -- |Delayed typechecker for a macro that is not yet instantiated.
-    itiFunctionInfo :: TypeInfo -> m (InternalTypeInfo m)
-  }
 
 makeFieldLabelsWith abbreviatedFieldLabels ''InternalTypeInfo
 makePrisms ''InternalTypeInfo
@@ -104,7 +104,7 @@ makePrisms ''InternalTypeInfo
 exprInfo :: InternalTypeInfo m
 exprInfo = Atomic (ExprInfo (Sens 0) D T 0)
 
-atomicCmdInfo :: Cxt -> ProbAnn -> TermAnn -> Double -> Double -> InternalTypeInfo m
+atomicCmdInfo :: Cxt m -> ProbAnn -> TermAnn -> Double -> Double -> InternalTypeInfo m
 atomicCmdInfo c p t e d = Atomic (CmdInfo c p t e d)
 
 -- |Cast an internal type info into an expression type info.
@@ -116,7 +116,7 @@ asExprInfo info =
         <*> preview (#typeInfo % #epsilon) info
 
 -- |Cast an internal type info into a command type info
-asCmdInfo :: InternalTypeInfo m -> Maybe (Cxt, ProbAnn, TermAnn, Double, Double)
+asCmdInfo :: InternalTypeInfo m -> Maybe (Cxt m, ProbAnn, TermAnn, Double, Double)
 asCmdInfo info =
   (,,,,) <$> preview (#typeInfo % #postContext) info
          <*> preview (#typeInfo % #probAnn) info
@@ -124,15 +124,12 @@ asCmdInfo info =
          <*> preview (#typeInfo % #epsilon) info
          <*> preview (#typeInfo % #delta) info
 
-type TypeChecker m = Cxt -> m (InternalTypeInfo m)
+type TypeChecker m = Cxt m -> m (InternalTypeInfo m)
 
 class TyAlg (h :: (* -> *) -> * -> *) where
   tyAlg :: MonadThrow m => Alg h (K (TypeChecker m))
 
 liftSum ''TyAlg
-
-instance TyAlg (EffF :&: Pos) where
-  tyAlg = tyAlgEff
 
 data TypeError =
   ExpectingAnExpression
@@ -146,6 +143,8 @@ data TypeError =
   | OutOfScopeVariable Name
   | SensitiveArrayIndex
   | SensitiveArraySize
+  | DivergedTypeInfo Name
+  | MergingMacroTypeInfo Name
   deriving (Show, Eq, Ord)
 
 data PositionAndTypeError = PTE {
@@ -158,100 +157,28 @@ instance Exception PositionAndTypeError
 throwTE :: MonadThrow m => Pos -> TypeError -> m a
 throwTE p e = throwM (PTE p e)
 
-mergeBranchCxt :: Cxt -> Cxt -> Cxt
-mergeBranchCxt =
-  M.merge M.dropMissing M.dropMissing merge
-  where merge = M.zipWithAMatched (\_ s1 s2 -> pure $ max s1 s2)
+whenMissing :: MonadThrow m => Pos -> Name -> InternalTypeInfo m -> m a
+whenMissing pos x _ = throwTE pos (DivergedTypeInfo x)
 
--- |Typecheck the commands fragment.
-tyAlgEff :: MonadThrow m => Alg (EffF :&: Pos) (K (TypeChecker m))
-tyAlgEff (Assign (V x) rhs :&: position) = K $ \cxt -> do
-  tyRhs <- unK rhs cxt
-  case asExprInfo tyRhs of
-    Nothing -> throwTE position ExpectingAnExpression
-    Just (s, p, t, e) ->
-      let cxt' = M.insert x s cxt in
-      return (atomicCmdInfo cxt' p t e 0)
-tyAlgEff (Branch cond c1 c2 :&: position) = K $ \cxt -> do
-  tyCond <- unK cond cxt
-  case asExprInfo tyCond of
-    Just (asSens -> Sens 0, D, t, 0) -> do
-      tyC1 <- unK c1 cxt
-      tyC2 <- unK c2 cxt
-      case (asCmdInfo tyC1, asCmdInfo tyC2) of
-        (Just (postCxt1, p1, t1, eps1, dlt1), Just (postCxt2, p2, t2, eps2, dlt2)) ->
-          let cxt' = mergeBranchCxt postCxt1 postCxt2 in
-          return $
-            atomicCmdInfo cxt'
-              (p1 <> p2)
-              (t <> t1 <> t2)
-              (max eps1 eps2)
-              (max dlt1 dlt2)
-        (Just _, Nothing) -> throwTE position ExpectingFalseBranchCommand
-        (Nothing, _) -> throwTE position ExpectingTrueBranchCommand
-    Just (_, P, _, _) ->
-      throwTE position BranchConditionProb
-    Just (_, D, _, _) ->
-      throwTE position BranchConditionSensitive
-    Nothing -> throwTE position ExpectingAnExpression
-tyAlgEff (While cond c :&: position) = K $ \cxt -> do
-  tyCond <- unK cond cxt
-  case asExprInfo tyCond of
-    Just (isNonSensitive -> True, D, tCond, _) -> do
-      tyC <- unK c cxt
-      case asCmdInfo tyC of
-        Just (postCxt, p, tBody, 0, 0) -> do
-          when (cxt /= postCxt) $
-            throwTE position CannotFindLoopInvariant
-          return $ atomicCmdInfo cxt p (tCond <> tBody) 0 0
-        Just (_, _, _, eps, delta) ->
-          throwTE position (LoopHasPrivacyCost eps delta)
-        Nothing ->
-          throwTE position ExpectingLoopBodyCommand
-    Just (_, D, _, _) ->
-      throwTE position BranchConditionSensitive
-    Just (_, P, _, _) ->
-      throwTE position BranchConditionProb
-    Nothing ->
-      throwTE position ExpectingAnExpression
+whenMatching :: MonadThrow m
+             => Pos
+             -> Name
+             -> InternalTypeInfo m
+             -> InternalTypeInfo m
+             -> m (InternalTypeInfo m)
+whenMatching pos x Macro{} Macro{} = throwTE pos (MergingMacroTypeInfo x)
+whenMatching pos x (Atomic ty1) (Atomic ty2) =
+  case (ty1, ty2) of
+    (ExprInfo s1 p1 t1 eps1, ExprInfo s2 p2 t2 eps2) ->
+      return . Atomic $ ExprInfo (s1 <> s2) (p1 <> p2) (t1 <> t2) (max eps1 eps2)
+    (CmdInfo cxt1 p1 t1 eps1 del1, CmdInfo cxt2 p2 t2 eps2 del2) -> do
+      cxt <- mergeBranchCxt pos cxt1 cxt2
+      return . Atomic $ CmdInfo cxt (p1 <> p2) (t1 <> t2) (max eps1 eps2) (max del1 del2)
+    _ -> throwTE pos (DivergedTypeInfo x) 
+whenMatching pos x _ _ = throwTE pos (DivergedTypeInfo x)
 
-tyAlgExpr :: MonadThrow m => Alg (ExprF :&: Pos) (K (TypeChecker m))
-tyAlgExpr (Deref (V x) :&: position) = K $ \cxt -> do
-  case M.lookup x cxt of
-    Nothing -> throwTE position (OutOfScopeVariable x)
-    Just ty -> (return . Atomic) (ExprInfo ty D T 0)
-tyAlgExpr (Index checkArr checkIdx :&: position) = K $ \cxt -> do
-  arrTy <- unK checkArr cxt
-  idxTy <- unK checkIdx cxt
-  case ( asExprInfo arrTy
-       , asExprInfo idxTy) of
-    ( Just (arrSens, arrProb, arrTerm, eps1)
-      , Just (idxSens, idxProb, idxTerm, eps2)) -> do
-      when (not $ isNonSensitive idxSens) $
-        throwTE position SensitiveArrayIndex
-      return . Atomic $
-        ExprInfo arrSens (arrProb <> idxProb) (arrTerm <> idxTerm) (eps1+eps2)
-    _ -> throwTE position ExpectingAnExpression
-tyAlgExpr (Resize checkArr checkSize :&: position) = K $ \cxt -> do
-  arrTy <- unK checkArr cxt
-  sizeTy <- unK checkSize cxt
-  case ( asExprInfo arrTy
-       , asExprInfo sizeTy) of
-    ( Just (arrSens, arrProb, arrTerm, eps1)
-      , Just (sizeSens, sizeProb, sizeTerm, eps2)) -> do
-      when (not $ isNonSensitive sizeSens) $
-        throwTE position SensitiveArraySize
-      return . Atomic $
-        ExprInfo arrSens (arrProb <> sizeProb) (arrTerm <> sizeTerm) (eps1+eps2)
-    _ -> throwTE position ExpectingAnExpression
-tyAlgExpr (ArrLit checkInners :&: position) = K $ \cxt -> do
-  innerTys <- mapM (\checkTy -> unK checkTy cxt) checkInners
-  case traverse asExprInfo innerTys of
-    Just innerExprTys ->
-      let totalSens = foldMap (\einfo -> einfo ^. _1) innerExprTys
-          aggrProbAnn = foldMap (\einfo -> einfo ^. _2) innerExprTys
-          aggrTermAnn = foldMap (\einfo -> einfo ^. _3) innerExprTys
-          totalEps = getSum $ foldMap (\einfo -> Sum (einfo ^. _4)) innerExprTys
-      in return . Atomic $
-           ExprInfo totalSens aggrProbAnn aggrTermAnn totalEps
-    Nothing -> throwTE position ExpectingAnExpression
+mergeBranchCxt :: MonadThrow m => Pos -> Cxt m -> Cxt m -> m (Cxt m)
+mergeBranchCxt p =
+  let missingHelper = M.traverseMaybeMissing (whenMissing p)
+      matchHelper = M.zipWithAMatched (whenMatching p)
+  in M.mergeA missingHelper missingHelper matchHelper
